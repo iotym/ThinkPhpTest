adbed4dcab593460696b9bd97e9dea49d66ff141
iotym <1640755684@qq.com>
2017-07-31 20:58:55 +0800

    因为common属于公共类 文件，不能直接访问，所可以使用一下方式访问

---
+++ application/common/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/31
+ * Time: 20:43
+ */
+namespace application\common\controller;
+
+class Index
+{
+    public function index()
+    {
+        return "this my frist thinkphp object";
+    }
+
+}
